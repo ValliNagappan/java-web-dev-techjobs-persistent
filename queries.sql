a96f71a5527fb332e6158f28d8968794142cd354
Valli N <vallisenthil_s@yahoo.com>
2021-03-28 17:18:07 -0500

    final commit

--- queries.sql
+++ queries.sql
@@ -1,7 +1,11 @@
 ## Part 1: Test it with SQL
 
 ## Part 2: Test it with SQL
+SELECT name
+FROM employer
+WHERE location = "St louis"
 
 ## Part 3: Test it with SQL
+DROP TABLE job
 
 ## Part 4: Test it with SQL
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,36 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @Size(min=3, max=20)
+    private String location;
+    @OneToMany
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<>();
 
+    public Employer(){}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 }
